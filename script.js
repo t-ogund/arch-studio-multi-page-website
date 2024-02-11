b2ef9a918d09c02d923163f4b9ee88aa0b25729d
Toye Ogundepo <ogundepo@gmail.com>
2024-02-11 16:41:51 -0600

    add links to menu

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu");
+const toggleMenu = document.querySelector(".open-menu");
 
 hamburgerMenu.addEventListener("click", () => {
     console.log("test")
+    toggleMenu.classList.toggle("hide");
 })
